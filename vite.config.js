717028f26892b4488359133212a56b54849b8003
chhayashah22 <your-email@example.com>
2024-08-11 15:13:30 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-'/api':'http://localhost:8083'
-    }
+  base: '/Certificate/', // Adjust this to match your GitHub Pages path
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8083',
+    },
   },
   plugins: [react()],
-
-})
+});
