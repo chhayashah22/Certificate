ccc40408aac29a3c33f936fbd56f185a09975ae1
chhayashah22 <your-email@example.com>
2024-08-11 15:23:14 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Certificate/', // Adjust this to match your GitHub Pages path
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8083',
-    },
+  server:{
+    proxy:{
+'/api':'http://localhost:8083'
+    }
   },
   plugins: [react()],
-});
+
+})
