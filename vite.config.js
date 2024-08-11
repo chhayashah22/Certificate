95379e95c3f369bda019e169590c31be9313bbb9
chhayashah22 <your-email@example.com>
2024-08-11 15:57:22 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  server:{
-    proxy:{
-'/api':'http://localhost:8083'
-    }
-  },
+  
   plugins: [react()],
 
 })
